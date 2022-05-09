1245c49cf722a646bec183e5a9879b0047b17b47
chocoshop <15ec112j@rikkyo.ac.jp>
2022-05-10 01:55:56 +0900

    Mutationを行うメソッドを追加した
    ログイン、予約、キャンセル

--- src/resolover.js
+++ src/resolover.js
@@ -50,4 +50,45 @@ module.exports = {
         );
         },
     },
+
+    Mutation: {
+        login: async (_, { email }, { dataSources }) => {
+            const user = dataSources.userAPI.findOrCreateUser({ email });
+            if (user) {
+                user.token = Buffer.from(email).toString();
+                return user;
+            }
+        },
+        bookTrip: async (_, { launchIds }, { dataSources }) => {
+            const results = await dataSources.userAPI.bookTrip({ launchIds });
+            const launches = await dataSources.launchAPI.getLaunchesByIds({launchIds});
+            
+            return {
+                success: results && results.length === launchIds.length,
+                message:
+                    results.length === launchIds.length
+                        ? 'trips booked successfully'
+                        : `the following launches couldn't be booked ${launchIds.filter(
+                            id => !results.includes(id)
+                        )}`,
+            }
+        },
+        cancelTrip: async (_, { launchId }, { dataSources }) => {
+            const result = dataSources.userAPI.cancelTrip({ launchId });
+
+            if (!result) {
+                return {
+                    success: false,
+                    message: 'failed to cancel trip',
+                }
+            }
+
+            const launch = dataSources.launchAPI.getLaunchById({ launchId });
+            return {
+                success: true,
+                message: 'trip cancelles',
+                launch: [launch],
+            }
+        }
+    }
   };
